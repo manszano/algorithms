494469fea0fcfd473e6e89e6b6097bfcc95895f9
manzano <matheusmanzano7@gmail.com>
2023-05-22 14:42:49 -0300

    bc solutions

--- beecrowd/De dentro para fora/Solution - 1.py
+++ beecrowd/De dentro para fora/Solution - 1.py
@@ -1,3 +1,9 @@
+
+#A sua impressora foi infectada por um vírus e está imprimindo de forma incorreta.
+# Depois de olhar para várias páginas impressas por um tempo, você percebe que ele está imprimindo cada linha de dentro para fora.
+# Em outras palavras, a metade esquerda de cada linha está sendo impressa a partir do meio da página até a margem esquerda.
+# Do mesmo modo, a metade direita de cada linha está sendo impressa à partir da margem direita e prosseguindo em direção ao centro da página.
+
 casos = int(input())
 
 for caso in range(casos):
